7104c686debcf955f1051edcb3853e33b5381967
쫑사마 <lodossw@gmail.com>
2022-06-25 16:24:09 +0900

    커밋 테스트

--- Day01/MySecondClass.java
+++ Day01/MySecondClass.java
@@ -7,7 +7,8 @@ public class MySecondClass { // 자바 클래스의 시작
 	
 	public static void main(String[] args) { // 메소드 시작
 	  // 메서드 블록 
-	  //-- .. 문장들 
+	  //-- .. 문장들
+	  System.out.println("Git Commit 후 변경해보기");
 	} // 메소드의 끝 
 	
 } // 자바 클래스의 끝 
