c925be73985fdd58ab6ff5821e332f378ff4f7b9
쫑사마 <lodossw@gmail.com>
2022-07-30 17:30:37 +0900

    하악하악 커밋

--- Day06/src/ScannerExample.java
+++ Day06/src/ScannerExample.java
@@ -4,12 +4,13 @@ public class ScannerExample {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
+//		Scanner sc = new Scanner(System.in);
 //		String input = sc.nextLine(); 
 //		System.out.println("입력값은 " + input + " 입니다.");
 		
 		//--- 하악하악 
-		
+
+		Scanner sc = new Scanner(System.in);
 		int width, height; 
 		
 		while(true) {
